a4e6f9b33e3c516af3859e2ac464480cb4dc147c
Pavlo Karas <kar.pavel123@gmail.com>
2023-07-15 13:12:52 +0300

    Complete second parts of this puzzle

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -8,16 +8,41 @@ enum GameElements {
 
 fn main() {
     let file_content = fs::read_to_string("./input").expect("file");
-    let game_result = start_the_game(&file_content);
-    println!("Result: {}", game_result);
+    let first_half_result = calculate_first_half(&file_content);
+    println!("Result: {}", first_half_result);
+    let second_half_result = calculate_second_half(&file_content);
+    println!("Second half: {}", second_half_result);
 }
 
-fn start_the_game(input: &str) -> u32 {
-    let mut count = 0u32;
+fn calculate_second_half(input: &str) -> u32 {
+    let mut count = 0;
     for line in input.trim().lines() {
         let choose: Vec<&str> = line.split_whitespace().collect();
         let (opponent_choose, my_choose) = match_choose(choose[0], choose[1]);
 
+        match (opponent_choose, my_choose) {
+            (GameElements::Rock, GameElements::Rock) => count += 3,
+            (GameElements::Paper, GameElements::Rock) => count += 1,
+            (GameElements::Scissors, GameElements::Rock) => count += 2,
+
+            (GameElements::Rock, GameElements::Paper) => count  += 4,
+            (GameElements::Paper, GameElements::Paper) => count += 5,
+            (GameElements::Scissors, GameElements::Paper) => count += 6,
+
+            (GameElements::Rock, GameElements::Scissors) => count += 8,
+            (GameElements::Paper, GameElements::Scissors) => count += 9,
+            (GameElements::Scissors, GameElements::Scissors) => count += 7,
+        }
+    }
+
+    count
+}
+
+fn calculate_first_half(input: &str) -> u32 {
+    let mut count = 0u32;
+    for line in input.trim().lines() {
+        let choose: Vec<&str> = line.split_whitespace().collect();
+        let (opponent_choose, my_choose) = match_choose(choose[0], choose[1]);
 
         match (&opponent_choose, &my_choose) {
             (GameElements::Rock, GameElements::Paper) => count += 6,
