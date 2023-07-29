71717f37d092fb0d85f52c3ad53d3a07384c8cd3
Pavlo Karas <kar.pavel123@gmail.com>
2023-07-29 16:36:53 +0300

    Second part of Tuning Trouble solved

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -3,19 +3,30 @@ use std::fs;
 fn main() {
     let first_part = solve_first_part();
     println!("First part: {}", first_part);
+
+    let second_part = solve_second_part();
+    println!("Second part: {}", second_part);
+}
+
+fn solve_second_part() -> u32 {
+    find_start_marker(14)
 }
 
 fn solve_first_part() -> u32 {
+    find_start_marker(4)
+}
+
+fn find_start_marker(distinct_characters: u32) -> u32 {
     let file_content = fs::read_to_string("input").unwrap();
 
     for x in 0..file_content.len() {
-        let buffer_part = &file_content[x..x + 4];
+        let buffer_part = &file_content[x..x + distinct_characters as usize];
         if is_marker(&buffer_part) {
-            return x as u32 + 4;
+            return x as u32 + distinct_characters;
         }
     }
 
-    69
+    panic!("Maybe your puzzle input is wrong");
 }
 
 fn is_marker(characters: &str) -> bool {
