d0f829c2c84d1c08e04c5d5b7047cf97aebd7b1d
Pavlo Karas <kar.pavel123@gmail.com>
2023-12-01 15:21:57 +0200

    refactor: day1 puzzles

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -9,71 +9,63 @@ fn main() {
 }
 
 fn solve_first_part(input: &str) -> u32 {
-    let mut calibration_sum = 0;
+    input
+        .lines()
+        .map(|l| {
+            let calibration_arr: Vec<_> = l.chars().filter(|c| c.is_digit(10)).collect();
 
-    for l in input.lines() {
-        let calibration_arr: Vec<_> = l
-            .split("")
-            .filter(|c| match c.parse::<u32>() {
-                Ok(_) => true,
-                Err(_) => false,
-            })
-            .collect();
+            let combined_values = format!(
+                "{}{}",
+                calibration_arr.first().expect("not empty vec"),
+                calibration_arr.last().expect("not empty vec")
+            );
 
-        let compbined_values = format!(
-            "{}{}",
-            calibration_arr.first().expect("not empty vec"),
-            calibration_arr.last().expect("not empty vec")
-        );
-
-        calibration_sum += compbined_values.parse::<u32>().expect("number");
-    }
-
-    calibration_sum
+            combined_values.parse::<u32>().expect("number")
+        })
+        .sum()
 }
 
 fn solve_second_part(input: &str) -> u32 {
-    let mut calibration_sum = 0;
+    input
+        .lines()
+        .map(|l| {
+            let digits = find_digits(&l);
+            let calibration_values = format!(
+                "{}{}",
+                digits.first().expect("not empty vec"),
+                digits.last().expect("not empty vec")
+            );
 
-    for l in input.lines() {
-        let digits = find_digits(&l);
-        let calibration_values = format!(
-            "{}{}",
-            digits.first().expect("not empty vec"),
-            digits.last().expect("not empty vec")
-        );
-
-        calibration_sum += calibration_values.parse::<u32>().expect("number");
-    }
-
-    calibration_sum
+            calibration_values.parse::<u32>().expect("number")
+        })
+        .sum()
 }
 
-fn find_digits(line: &str) -> Vec<&str> {
+fn find_digits(line: &str) -> Vec<u32> {
     let mut digit_str = vec![];
     let calibration_symbols = [
         "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
     ];
 
     for i in 0..line.len() {
-        for symbol in calibration_symbols.iter() {
+        calibration_symbols.iter().for_each(|symbol| {
             if line[i..].starts_with(symbol) {
                 match *symbol {
-                    "one" => digit_str.push("1"),
-                    "two" => digit_str.push("2"),
-                    "three" => digit_str.push("3"),
-                    "four" => digit_str.push("4"),
-                    "five" => digit_str.push("5"),
-                    "six" => digit_str.push("6"),
-                    "seven" => digit_str.push("7"),
-                    "eight" => digit_str.push("8"),
-                    "nine" => digit_str.push("9"),
+                    "one" => digit_str.push(1),
+                    "two" => digit_str.push(2),
+                    "three" => digit_str.push(3),
+                    "four" => digit_str.push(4),
+                    "five" => digit_str.push(5),
+                    "six" => digit_str.push(6),
+                    "seven" => digit_str.push(7),
+                    "eight" => digit_str.push(8),
+                    "nine" => digit_str.push(9),
                     _ => (),
                 };
-            } else if let Ok(_) = line[i..i + 1].parse::<u32>() {
-                digit_str.push(&line[i..i + 1]);
+            } else if let Ok(num) = line[i..i + 1].parse::<u32>() {
+                digit_str.push(num);
             }
-        }
+        })
     }
 
     digit_str
