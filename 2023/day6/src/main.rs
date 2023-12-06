8cbadfda78ed8dbf9b1eea0522e9cc10ba411d69
Pavlo Karas <kar.pavel123@gmail.com>
2023-12-06 10:07:48 +0200

    Solve the second part of the day 6

--- 2023/day6/src/main.rs
+++ 2023/day6/src/main.rs
@@ -2,6 +2,9 @@ fn main() {
     let input1 = include_str!("./input1");
     let first_part_result = first_part(&input1);
     println!("First part: {}", first_part_result);
+
+    let second_part_result = second_part(&input1);
+    println!("Second part: {}", second_part_result);
 }
 
 fn first_part(input: &str) -> u32 {
@@ -39,9 +42,37 @@ fn first_part(input: &str) -> u32 {
         .unwrap()
 }
 
+fn second_part(input: &str) -> u32 {
+    let records = input
+        .lines()
+        .map(|l| {
+            l.split(":").collect::<Vec<_>>()[1]
+                .split_whitespace()
+                .collect::<Vec<_>>()
+                .join("")
+                .parse::<u64>()
+                .unwrap()
+        })
+        .collect::<Vec<_>>();
+
+    let (time, distance) = (records[0], records[1]);
+
+    let range = 1..time;
+    range.fold(0u32, |win_rounds, speed| {
+        let remign_distance = time - speed;
+        let pass_distance = remign_distance * speed;
+
+        if pass_distance > distance {
+            win_rounds + 1
+        } else {
+            win_rounds
+        }
+    })
+}
+
 #[cfg(test)]
 mod test {
-    use crate::first_part;
+    use crate::{first_part, second_part};
 
     #[test]
     fn test_first_part() {
@@ -50,5 +81,14 @@ Time:      7  15   30
 Distance:  9  40  200";
 
         assert_eq!(first_part(&input), 288);
+    }
+
+    #[test]
+    fn test_second_part() {
+        let input = "\
+Time:      7  15   30
+Distance:  9  40  200";
+
+        assert_eq!(second_part(&input), 71503);
     }
 }
