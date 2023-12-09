dcd00945285ca746937aaf2a51a4e0caf8d620de
Pavlo Karas <kar.pavel123@gmail.com>
2023-12-09 23:11:56 +0200

    Solve the second second part of the day 8

--- 2023/day8/src/main.rs
+++ 2023/day8/src/main.rs
@@ -4,6 +4,9 @@ fn main() {
     let input1 = include_str!("./input1");
     let first_part_result = first_part(&input1);
     println!("First part: {}", first_part_result);
+
+    let second_part_result = second_part(&input1);
+    println!("Second part: {}", second_part_result);
 }
 
 fn first_part(input: &str) -> u32 {
@@ -44,9 +47,73 @@ fn first_part(input: &str) -> u32 {
     steps_to_reach_finish as u32
 }
 
+fn second_part(input: &str) -> u64 {
+    let splitted_map = input.split("\n\n").collect::<Vec<_>>();
+    let navigation_instuction = splitted_map[0];
+    let navigation_network = splitted_map[1]
+        .lines()
+        .fold(HashMap::new(), |mut nodes, l| {
+            let splitted_position = l
+                .split(&['=', '(', ',', ')', ' '][..])
+                .filter(|&e| e != "")
+                .collect::<Vec<_>>();
+
+            nodes.insert(
+                splitted_position[0],
+                (splitted_position[1], splitted_position[2]),
+            );
+
+            nodes
+        });
+
+    let path_lengths = navigation_network
+        .iter()
+        .filter(|path| path.0.ends_with("A"))
+        .map(|n| {
+            let mut i = 0;
+            let mut current = n;
+
+            while !current.0.ends_with("Z") {
+                let route_index = i % navigation_instuction.len();
+                let route_path = &navigation_instuction[route_index..route_index + 1];
+
+                current = match route_path {
+                    "L" => navigation_network.get_key_value(current.1 .0).unwrap(),
+                    "R" => navigation_network.get_key_value(current.1 .1).unwrap(),
+                    _ => unreachable!(),
+                };
+
+                i += 1;
+            }
+
+            i as u64
+        })
+        .collect::<Vec<_>>();
+
+
+        lcm_of_vector(&path_lengths)
+}
+
+fn lcm_of_vector(numbers: &Vec<u64>) -> u64 {
+    numbers.iter().fold(1, |lcm, &num| lcm_of_two(lcm, num))
+}
+
+fn lcm_of_two(a: u64, b: u64) -> u64 {
+    a / gcd(a, b) * b // LCM formula using GCD
+}
+
+fn gcd(mut a: u64, mut b: u64) -> u64 {
+    while b != 0 {
+        let t = b;
+        b = a % b;
+        a = t;
+    }
+    a
+}
+
 #[cfg(test)]
 mod test {
-    use crate::first_part;
+    use crate::{first_part, second_part};
 
     #[test]
     fn test_first_part() {
@@ -70,5 +137,22 @@ ZZZ = (ZZZ, ZZZ)";
 
         assert_eq!(first_part(&input1), 2);
         assert_eq!(first_part(&input2), 6);
+    }
+
+    #[test]
+    fn test_second_part() {
+        let input = "\
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)";
+
+        assert_eq!(second_part(&input), 6);
     }
 }
